74b731a6d44e43d2e189ad206f404af09cdb2c64
olusolaisrealoluwasemilore100-beep <olusolaisrealoluwasemilore100@gmail.com>
2025-08-16 01:39:09 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,6 +1,6 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
-  OWNER_NUMBER: "263719647303", // Put Owner Number Here 
+  SESSION_ID: "SUBZERO-MD;;;2FsVnCKY#RXmpm60GfKX_sa0NTMyn7_qVdFXDIPJ_oZ4_iwtNOAU", // Put Session Id Here Inside "*"
+  OWNER_NUMBER: "2348169546065", // Put Owner Number Here 
   PREFIX: "." // Default Prefix You Can Change To .,*!?+-&%#@
 };
 
